41c852514a5da145efab875da8885f57515e7cd6
Seong-uk-An <asu970602@naver.com>
2021-08-28 00:29:58 +0900

    MainStack

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,5 +2,7 @@ import Login from "./Login";
 import SignUp from "./SignUp";
 import Channel from "./Channel";
 import ChannelCreation from "./ChannelCreation";
+import ChannelList from "./ChannelList";
+import Profile from "./Profile";
 
-export { Login, SignUp, Channel, ChannelCreation };
+export { Login, SignUp, Channel, ChannelCreation, ChannelList, Profile };
