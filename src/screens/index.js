88ffca4dea5193987ea84a817e2d6276857eb80b
Seong-uk-An <asu970602@naver.com>
2021-08-28 00:21:10 +0900

    main

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,4 +1,6 @@
 import Login from "./Login";
 import SignUp from "./SignUp";
+import Channel from "./Channel";
+import ChannelCreation from "./ChannelCreation";
 
-export { Login, SignUp };
+export { Login, SignUp, Channel, ChannelCreation };
