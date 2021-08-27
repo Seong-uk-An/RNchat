a3d1c2c0e2f11bc0a226f586f8596beca5f28ed9
Seong-uk-An <asu970602@naver.com>
2021-08-27 15:46:19 +0900

    login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,22 +1,89 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
-import { Button } from "react-native";
-import { Image } from "../components";
+import { Image, Input, Button } from "../components";
 import { images } from "../utils/images";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { validateEmail, removeWhitespace } from "../utils/common";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.background};
+  padding: 20px;
+`;
+const ErrorText = styled.Text`
+  align-items: flex-start;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 10px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.errorText};
 `;
 
 const Login = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    setDisabled(!(email && password && !errorMessage));
+  }, [email, password, errorMessage]);
+
+  const _handleEmailChange = (email) => {
+    const changedEmail = removeWhitespace(email);
+    setEmail(changedEmail);
+    setErrorMessage(
+      validateEmail(changedEmail) ? "" : "Please verify your email"
+    );
+  };
+  const _handlePasswordChange = (password) => {
+    setPassword(removeWhitespace(password));
+  };
+  const _handleLoginButtonPress = () => {};
+
   return (
-    <Container>
-      <Image url={images.logo} />
-      <Button title="Sign Up" onPress={() => navigation.navigate("SignUp")} />
-    </Container>
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flex: 1 }}
+      extraScrollHeight={20}
+    >
+      <Container>
+        <Image url={images.logo} />
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={_handleEmailChange}
+          onSubmitEditing={() => passwordRef.current.focus()}
+          placeholder="Email"
+          returnKeyType="next"
+        />
+        <Input
+          ref={passwordRef}
+          label="Password"
+          value={password}
+          onChangeText={_handlePasswordChange}
+          onSubmitEditing={_handleLoginButtonPress}
+          placeholder="Password"
+          returnKeyType="done"
+          isPassword
+        />
+        <ErrorText>{errorMessage}</ErrorText>
+        <Button
+          title="Log in"
+          onPress={_handleLoginButtonPress}
+          disabled={disabled}
+        />
+        <Button
+          title="Sign up with Email"
+          onPress={() => {
+            navigation.navigate("SignUp");
+          }}
+          isFilled={false}
+        />
+      </Container>
+    </KeyboardAwareScrollView>
   );
 };
 
