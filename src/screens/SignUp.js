5ffad46b073b758cec416b7a5b7a8e76623226cd
Seong-uk-An <asu970602@naver.com>
2021-08-28 00:11:04 +0900

    spinner

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useContext } from "react";
 import styled from "styled-components";
 import { Image, Input, Button } from "../components";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
@@ -6,6 +6,7 @@ import { validateEmail, removeWhitespace } from "../utils/common";
 import { images } from "../utils/images";
 import { Alert } from "react-native";
 import { signup } from "../utils/firebase";
+import { ProgressContext } from "../contexts";
 
 const Container = styled.View`
   flex: 1;
@@ -24,6 +25,7 @@ const ErrorText = styled.Text`
 `;
 
 const SignUp = () => {
+  const { spinner } = useContext(ProgressContext);
   const [photoUrl, setPhotoUrl] = useState(images.photo);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -65,11 +67,14 @@ const SignUp = () => {
 
   const _handleSignupButtonPress = async () => {
     try {
+      spinner.start();
       const user = await signup({ email, password, name, photoUrl });
       console.log(user);
       Alert.alert("Signup Success", user.email);
     } catch (e) {
       Alert.alert("Signup Error", e.message);
+    } finally {
+      spinner.stop();
     }
   };
 
